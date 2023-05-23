06fcd68cdd00e0f748d9408d11de850114918554
Kangzlz <127727428+Kangzlz@users.noreply.github.com>
2023-05-24 01:13:57 +0800

    7-1

---
+++ .vscode/daima7-3.c
@@ -0,0 +1,11 @@
+/*显示字符型和整形的长度*/
+#include<stdio.h>
+int main(void){
+
+    printf("sizeof(char)=%u\n",(unsigned)sizeof(char));
+    printf("sizeof(shor)=%u\n",(unsigned)sizeof(short));
+    printf("sizeof(int)=%u\n",(unsigned)sizeof(int));
+    printf("sizeof(long)=%u\n",(unsigned)sizeof(long));
+
+    return 0;
+}
